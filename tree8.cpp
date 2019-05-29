7b914f420c31735eb91d2f6d6ba1e28d58994df8
Harshit Mogalapalli <38498339+harshimogalapalli@users.noreply.github.com>
2019-05-29 21:54:25 +0530

    Create tree8.cpp

---
+++ tree8.cpp
@@ -0,0 +1,26 @@
+int height(Node * root){
+    int l,r;
+    
+    if(root==NULL)return 0;
+    
+    l=height(root->left);
+    r=height(root->right);
+    if(l>r)return l+1;
+    return r+1;
+}
+int getw(Node *root,int l){
+    if(root==NULL)return 0;
+    if(l==1)cout<<root->data<<" ";
+    else { getw(root->left,l-1);
+    getw(root->right,l-1);
+    }
+    
+}
+void reversePrint(Node* root)
+{
+    int h=height(root);
+int mw=0;int w;
+for(int i=h;i>=1;i--){
+    getw(root,i);
+}
+}

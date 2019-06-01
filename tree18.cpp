e10fafed5bfea801908f92feb7e78d8d8fe46b0d
Harshit Mogalapalli <38498339+harshimogalapalli@users.noreply.github.com>
2019-06-02 00:09:46 +0530

    Create tree18.cpp

---
+++ tree18.cpp
@@ -0,0 +1,8 @@
+void printKdistance(Node *root, int k)
+{if(root==NULL)return;
+if(k==0)cout<<root->data<<" ";
+else{
+    printKdistance(root->left,k-1);
+    printKdistance(root->right,k-1);
+}
+}

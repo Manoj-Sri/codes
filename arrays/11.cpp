c208b5835165942ddaea5b31c5912db5a987bbde
Harshit Mogalapalli <38498339+harshimogalapalli@users.noreply.github.com>
2019-06-04 22:46:04 +0530

    Create 11.cpp

---
+++ arrays/11.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	//code
+	int t,n;
+	cin>>t;
+	while(t--){
+	    cin>>n;
+	    int arr[n];
+	    for(int i=0;i<n;i++)cin>>arr[i];
+	    sort(arr,arr+n);
+	    int mind=34563456;
+	    for(int i=0;i<n-1;i++){
+	        if(mind>(arr[i+1]-arr[i]))mind=(arr[i+1]-arr[i]);
+	    }
+	    cout<<mind<<endl;
+	}
+	return 0;
+}

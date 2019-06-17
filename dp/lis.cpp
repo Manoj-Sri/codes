ce15a5100a147d942286076f1d6518c5ca559fa3
Harshit Mogalapalli <38498339+harshimogalapalli@users.noreply.github.com>
2019-06-18 02:27:11 +0530

    Create lis.cpp

---
+++ dp/lis.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>  
+using namespace std; 
+int lis( int arr[], int n )  
+{ int lis[n]; 
+    lis[0] = 1;    
+  for (int i = 1; i < n; i++ )  
+    {  lis[i] = 1; 
+        for (int j = 0; j < i; j++ )   
+            if ( arr[i] > arr[j] && lis[i] < lis[j] + 1)  
+                lis[i] = lis[j] + 1;  
+    } 
+   return *max_element(lis, lis+n); 
+}  
+int main()  
+{  
+    
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        int arr[n];
+        for(int i=0;i<n;i++)cin>>arr[i];
+        int k=lis(arr,n);
+        cout<<k<<endl; }
+    
+    return 0;  
+}

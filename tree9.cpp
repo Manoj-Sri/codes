f5587c469753d367012cab9fc649cd534ea3ca56
Harshit Mogalapalli <38498339+harshimogalapalli@users.noreply.github.com>
2019-07-22 18:49:56 +0530

    Update tree9.cpp

--- tree9.cpp
+++ tree9.cpp
@@ -24,7 +24,49 @@ void printSpiral(Node* root)
 {
     int h=height(root);
 int mw=0;int w;bool rota=false;
-for(int i=1;i<=h;i--){
+for(int i=1;i<=h;i++){
     getw(root,i,rota);rota=~rota;
 }
 }
+//optimised using two stacks in O(n)
+
+if (root == NULL) 
+        return; // NULL check 
+  
+    // Create two stacks to store alternate levels 
+    stack<struct node*> s1; // For levels to be printed from right to left 
+    stack<struct node*> s2; // For levels to be printed from left to right 
+  
+    // Push first level to first stack 's1' 
+    s1.push(root); 
+  
+    // Keep printing while any of the stacks has some nodes 
+    while (!s1.empty() || !s2.empty()) { 
+        // Print nodes of current level from s1 and push nodes of 
+        // next level to s2 
+        while (!s1.empty()) { 
+            struct node* temp = s1.top(); 
+            s1.pop(); 
+            cout << temp->data << " "; 
+  
+            // Note that is right is pushed before left 
+            if (temp->right) 
+                s2.push(temp->right); 
+            if (temp->left) 
+                s2.push(temp->left); 
+        } 
+  
+        // Print nodes of current level from s2 and push nodes of 
+        // next level to s1 
+        while (!s2.empty()) { 
+            struct node* temp = s2.top(); 
+            s2.pop(); 
+            cout << temp->data << " "; 
+  
+            // Note that is left is pushed before right 
+            if (temp->left) 
+                s1.push(temp->left); 
+            if (temp->right) 
+                s1.push(temp->right); 
+        } 
+    } 
